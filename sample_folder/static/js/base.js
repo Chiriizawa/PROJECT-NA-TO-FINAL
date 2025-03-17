5820434876dd26f40165f563b975f33f838e029b
unknown <bergoniaraymund@gmail.com>
2025-03-17 23:49:18 +0800

    first

--- sample_folder/static/js/base.js
+++ sample_folder/static/js/base.js
@@ -1,4 +1,14 @@
-function toggleSidebar() {
-    const sidebar = document.getElementById("sidebar");
-    sidebar.classList.toggle("translate-x-full");
+function toggleDropdown() {
+    var dropdown = document.getElementById("profileDropdown");
+    dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
 }
+
+// Close the dropdown when clicking outside of it
+document.addEventListener("click", function(event) {
+    var profileIcon = document.querySelector(".profile-icon");
+    var dropdown = document.getElementById("profileDropdown");
+
+    if (!profileIcon.contains(event.target) && !dropdown.contains(event.target)) {
+        dropdown.style.display = "none";
+    }
+});
